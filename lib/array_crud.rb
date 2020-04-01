be68a9d22496fa3240b7a97a7a48ff4d0ca1b405
Steven Topazio <sat@northpointprinting.com>
2020-04-01 13:51:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["luna", "hercules"]
-  << abbie 
+ add_element_to_start_of_array << abbie 
 end
 
 def add_element_to_start_of_array(array, element)
